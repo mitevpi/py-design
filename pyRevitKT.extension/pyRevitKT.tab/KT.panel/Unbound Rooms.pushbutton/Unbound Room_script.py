f806fbe956991fea562c0fabd22b5955d50fe939
Petar Mitev <pmitev@kierantimberlake.com>
2017-09-11 12:05:26 -0400

    Functional

--- pyRevitKT.extension/pyRevitKT.tab/KT.panel/Unbound Rooms.pushbutton/Unbound Room_script.py
+++ pyRevitKT.extension/pyRevitKT.tab/KT.panel/Unbound Rooms.pushbutton/Unbound Room_script.py
@@ -1,6 +1,6 @@
-"""Finds Unbound Rooms."""
+"""Find& Delete Unbound Rooms."""
 
-__title__ = 'Unbound\nRooms\nCollector'
+__title__ = 'Unbound\nRooms\nPurge'
 __author__ = 'Petar Mitev'
 
 
@@ -14,13 +14,24 @@ from Autodesk.Revit.DB.Mechanical import Space
 doc = __revit__.ActiveUIDocument.Document
 
 # Creating collector instance and collecting all the walls from the model
-rms = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms)
+rms = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms).WhereElementIsNotElementType()
 
 # Iterate over rooms and collect Area data
+
+t = Transaction(doc, "Evaluate & Delete Rooms")
+t.Start()
+list = []
 
 for room in rms:
     area_param = room.LookupParameter('Area').AsDouble()
     if area_param < 0.1:
         name_param = room.LookupParameter('Name')
         id_param = room.Id
+        uid_param = room.UniqueId
+        list.append(id_param)
         print(name_param.AsString(),area_param,id_param.IntegerValue)
+
+for id in list:
+    doc.Delete(id)
+
+t.Commit()
