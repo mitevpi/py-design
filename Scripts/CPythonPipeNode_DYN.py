2e58238f58f23e0c08da523d26cfd188e1b879eb
Petar Mitev <pmitev@kierantimberlake.com>
2017-10-11 15:59:33 -0400

    CSV writer integrated into pipe node

--- Scripts/CPythonPipeNode_DYN.py
+++ Scripts/CPythonPipeNode_DYN.py
@@ -5,26 +5,31 @@ import csv
 import clr
 clr.AddReference('ProtoGeometry')
 from Autodesk.DesignScript.Geometry import *
-clr.AddReference('RevitNodes')
-clr.AddReference('RevitServices')
-clr.AddReference('RevitAPI')
 
 str = IN[0]
 working_dir = IN[1]
 data_in = IN[2]
 var_list = []
 
+#define the output files
 script_file = IN[1] + "\PYT3_script.py"
 data_file = working_dir + "/data.csv"
+
+#write data in to csv
+with open(data_file,'wb') as dataFile:
+    wr = csv.writer(dataFile, dialect='excel')
+    wr.writerows([data_in])
 		
-#start stdout
+#start stdout for creating a .py from the input "string"
 stdsave = sys.stdout
 fout = open(script_file,'w')
 sys.stdout = fout
 
+#create a results.txt file
 result_file = working_dir + "/results.txt"
 string_formatted = str.replace("working_directory", working_dir + "\\results.txt")
 
+#write input "string" to .py 
 print(string_formatted)
 sys.stdout = stdsave
 fout.close()
@@ -35,11 +40,13 @@ with open(data_file) as csvfile:
 	for row in reader:
 		var_list.append(row)
 
-#put directory parameter in
+#run .py via CMD instance
 pyt3_string = '"cmd.exe /c "cd/DIRECTORY&&python PYT3_script.py"'
 pyt3_string_formatted = pyt3_string.replace("DIRECTORY", working_dir)
 pyt3_string_formatted2 = pyt3_string_formatted.replace("C:\\", "")
 
+#add a sys path to the created .py directory
 os.system(pyt3_string_formatted2)
 
+#define outputs
 OUT = script_file, working_dir, result_file, string_formatted, var_list
