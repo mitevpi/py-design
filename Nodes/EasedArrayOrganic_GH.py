f9ff398ef7abbab50efa92f18b0f619a81bfcf96
Petar Mitev <pmitev@kierantimberlake.com>
2017-11-02 11:05:18 -0400

    0.66 Rollback

--- Nodes/EasedArrayOrganic_GH.py
+++ Nodes/EasedArrayOrganic_GH.py
@@ -6,7 +6,6 @@
         a: The Koch curve, as a list of lines.
 """
 import Rhino.Geometry as rg
-import rhinoscriptsyntax as rs
 import math
 import Rhino
 
@@ -19,19 +18,21 @@ linkedLinesOutsideEdge = []
 linkedLinesStraightOutsideEdge = []
 linkedCurvesStraight = []
 intersectionPoints = []
-intersectionPointsPanel = []
 spacings = []
 profileSections = []
 profileSectionsTop = []
 intersectionPlanes = []
 intersectionPlanesTop = []
 intersectionPlanesBottom = []
-panel_surfaces = []
+surface_intersections2 = []
+surface_intersections = []
 angle_list_bot = []
 angle_list_top = []
 lengthAtPt = 0
 finalLengthAtPt = 0
 totalLengthAtPt = 0
+intersectionCirclesTop = []
+intersectionCirclesBottom = []
 go = True
 
 profileBackEdgeLength = profileBackEdge.GetLength()
@@ -46,15 +47,15 @@ while totalLengthAtPt <= topCurve.GetLength() and go:
     tempProfileCurve = profileCurve.Duplicate()
     
     if lengthAtPt == 0:
-        print 'lengthAtPoint is 0!'
         topCurvePoint = topCurve.PointAtLength( 0 )
         topCurveParameter = topCurve.ClosestPoint( topCurvePoint )[1]
         bottomCurvePoint = bottomCurve.PointAtLength( 0 )
         bottomCurveParameter = bottomCurve.ClosestPoint( bottomCurvePoint )[1]
     else:
-        print 'lengthAtPoint is not 0!'
         circleTop = rg.Circle( topCurvePoint, lengthAtPt ).ToNurbsCurve()
+        intersectionCirclesTop.append( circleTop )
         circleBottom = rg.Circle( bottomCurvePoint, lengthAtPt ).ToNurbsCurve()
+        intersectionCirclesBottom.append( circleBottom )
         intersectionsTop = rg.Intersect.Intersection.CurveCurve( topCurve, circleTop, 0, 0 )
         intersectionsBottom = rg.Intersect.Intersection.CurveCurve( bottomCurve, circleBottom, 0, 0 )
         if intersectionsBottom and intersectionsTop:
@@ -65,7 +66,7 @@ while totalLengthAtPt <= topCurve.GetLength() and go:
             topCurveParameter = sortedListTop[0].ParameterA
             bottomCurvePoint = sortedListBottom[0].PointA
             bottomCurveParameter = sortedListBottom[0].ParameterA
-  
+      
     circleOutsideEdgePointTop = rg.Circle( topCurvePoint, profileBackEdgeLength ).ToNurbsCurve()
     circleOutsideEdgePointBottom = rg.Circle( bottomCurvePoint, profileBackEdgeLength ).ToNurbsCurve()
     intersectionsOutsideEdgeTop = rg.Intersect.Intersection.CurveCurve( topCurve, circleOutsideEdgePointTop, 0, 0 )
@@ -84,8 +85,17 @@ while totalLengthAtPt <= topCurve.GetLength() and go:
     planeBottom = bottomCurve.PerpendicularFrameAt( bottomCurveParameter )[1]
     planeAvg = rg.Plane( ( bottomCurvePoint+topCurvePoint )/2, ( planeTop.XAxis+planeBottom.XAxis ) / 2, (planeTop.YAxis+planeBottom.YAxis)/2 )
 
+    #plane/curve intersections
     intersections = rg.Intersect.Intersection.CurvePlane( topCurve, planeBottom, 0 )
-    
+
+    #plane/surface intersections
+    intersectionSrf = rg.Intersect.Intersection.BrepPlane(baseSurface, planeBottom, 0)
+
+
+    if intersectionSrf.Count > 0:
+        surface_intersections.append( sorted(intersectionSrf[1], key=lambda x: planeBottom.Origin.DistanceTo(x.PointAtStart), reverse=False)[0] )
+
+    #sort main array intersections
     if intersections:
         sortedListTopIntersection = sorted(intersections.Item, key=lambda x: x.PointA.DistanceTo( bottomCurvePoint ))
         linkedLineStraightTopPoint = sortedListTopIntersection[0].PointA
@@ -104,20 +114,12 @@ while totalLengthAtPt <= topCurve.GetLength() and go:
         planeAvgBack = rg.Plane( ( bottomCurvePointOutsideEdge+topCurvePointOutsideStraightEdge )/2, ( planeTop.XAxis+planeBottom.XAxis ) / 2, (planeTop.YAxis+planeBottom.YAxis)/2 )
     
     #intersection check for main array
-    if rg.Intersect.Intersection.CurvePlane( attractor, planeAvg, 0 ):
-        intersectionPoints.append(  rg.Intersect.Intersection.CurvePlane( attractor, planeAvg, 0 ).Item[0].PointA )
+    attractorIntersections = rg.Intersect.Intersection.CurvePlane( attractor, planeAvg, 0 )
+    if attractorIntersections:    
+        intersectionPoints.append(  sorted(attractorIntersections.Item, key=lambda x: planeAvg.Origin.DistanceTo(x.PointA), reverse=False)[0].PointA )
+        #intersectionPoints.append(  rg.Intersect.Intersection.CurvePlane( attractor, planeAvg, 0 ).Item[0].PointA )
     else:
         go = False
-
-    #check for panel loft attractor curve
-    if panel_toggle == False:
-        #intersection check for panel dims
-        if rg.Intersect.Intersection.CurvePlane( attractor_panel, planeAvg, 0 ):
-            intersectionPointsPanel.append(  rg.Intersect.Intersection.CurvePlane( attractor_panel, planeAvg, 0 ).Item[0].PointA )
-        if rg.Intersect.Intersection.CurvePlane( attractor_panel, planeAvgBack, 0 ):
-            intersectionPointsPanel.append(  rg.Intersect.Intersection.CurvePlane( attractor_panel, planeAvgBack, 0 ).Item[0].PointA )          
-        else:
-            go = False
 
     #normalize numbers for main array spacing    
     spacing = ( round( ( minSpacing + ( ( ( intersectionPoints[-1].Z - bottomCurvePoint.Z )/( topCurvePoint.Z - bottomCurvePoint.Z ) ) * ( maxSpacing-minSpacing ) ) ) * roundTo ) / roundTo )
@@ -153,16 +155,16 @@ while totalLengthAtPt <= topCurve.GetLength() and go:
         profileSectionsTop.append(tempProfileCurveTop)
 
         #rotate bottom section curves
-        v1 = rs.VectorCreate(profileBackEdge.PointAtStart, profileBackEdge.PointAtEnd)
-        v2 = rs.VectorCreate(bottomCurvePoint, rg.NurbsCurve.CreateFromLine(linkedLineStraightOutsideEdge).PointAt(0))
+        v1 = rg.Line(profileBackEdge.PointAtStart, profileBackEdge.PointAtEnd).Direction
+        v2 = rg.Line(bottomCurvePoint, rg.NurbsCurve.CreateFromLine(linkedLineStraightOutsideEdge).PointAt(0)).Direction
         y_axis_vector = rg.Vector3d(0, 0, 1)
-        vector_angle = Rhino.RhinoMath.ToRadians(rs.VectorAngle(v1, v2))
+        vector_angle = Rhino.RhinoMath.ToRadians(rg.Vector3d.VectorAngle(v1, v2))
         angle_list_bot.append(vector_angle)
         tempProfileCurve.Rotate(vector_angle, y_axis_vector, bottomCurvePoint)
 
         #rotate top section curves
-        v3 = rs.VectorCreate(topPoint, rg.NurbsCurve.CreateFromLine(linkedLineStraightOutsideEdge).PointAt(1))
-        vector_angle = Rhino.RhinoMath.ToRadians(rs.VectorAngle(v1, v3))
+        v3 = rg.Line(topPoint, rg.NurbsCurve.CreateFromLine(linkedLineStraightOutsideEdge).PointAt(1)).Direction
+        vector_angle = Rhino.RhinoMath.ToRadians(rg.Vector3d.VectorAngle(v1, v3))
         angle_list_top.append(vector_angle)
         tempProfileCurveTop.Rotate(vector_angle, y_axis_vector, topPoint)  
 
@@ -174,55 +176,6 @@ while totalLengthAtPt <= topCurve.GetLength() and go:
     
     if iter > 10000:
         go = False
-
-#normalize numbers for curve parameters
-curve_params_list = []
-for i in intersectionPointsPanel:
-    x = abs((((i.Z - bottomCurvePoint.Z) * (1 - 0)) / (topCurvePoint.Z - bottomCurvePoint.Z)) + 0)
-    if x > 1:
-        x = 1
-    if x < 0:
-        x = 0
-    curve_params_list.append(x)
-
-
-#make panels, initial indeces for the "panel" space is 1,2. +2,+2 after that
-index_a = 1
-index_b = 2
-non_euclid_panels = []
-panel_solid_list = []
-
-#loop for panel creation
-while len(linkedCurvesStraight) > index_b:
-    #create euclidean panels
-    if panel_toggle2 == True:
-        lofts = rs.AddLoftSrf([linkedCurvesStraight[index_a], linkedCurvesStraight[index_b]])
-        panel_surfaces.append(lofts[0])
-        panel_solid_list.append(rs.OffsetSurface(lofts[0], panel_thickness, tolerance=None, both_sides=False, create_solid=True))
-    #create non-euclidean panels
-    elif panel_toggle == True:
-        segment_a = linkedCurvesStraight[index_a].Split(panel_edgeA)
-        segment_b = linkedCurvesStraight[index_b].Split(panel_edgeB)
-        non_euclid_segments = []
-        non_euclid_segments.append(segment_a[0])
-        non_euclid_segments.append(segment_b[0])
-        lofts2 = rs.AddLoftSrf([non_euclid_segments[0], non_euclid_segments[1]], loft_type=1)
-        non_euclid_panels.append(lofts2[0])
-        panel_solid_list.append(rs.OffsetSurface(lofts2[0], panel_thickness, tolerance=None, both_sides=False, create_solid=True))
-    elif panel_toggle == False:
-        try:
-            segment_a = linkedCurvesStraight[index_a].Split(curve_params_list[index_a])
-            segment_b = linkedCurvesStraight[index_b].Split(curve_params_list[index_b])
-            lofts2 = rs.AddLoftSrf([segment_a[0], segment_b[0]], loft_type=1)
-            non_euclid_panels.append(lofts2[0])
-            panel_solid_list.append(rs.OffsetSurface(lofts2[0], panel_thickness, tolerance=None, both_sides=False, create_solid=True))
-        except:
-             pass
-        
-    #move to next "panel" space
-    index_a = index_a + 2
-    index_b = index_b + 2
-
 
 final_length = "Final Length: {}".format( finalLengthAtPt )
 short_by = "Short by: {}".format( topCurve.GetLength() - finalLengthAtPt - profileBackEdgeLength )
