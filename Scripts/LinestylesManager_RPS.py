49b8ecea09a2c4689bcd998897181206447eee67
Petr Mitev <pmitev@kierantimberlake.com>
2017-11-12 12:18:12 -0500

    Working RPW Forms

--- Scripts/LinestylesManager_RPS.py
+++ Scripts/LinestylesManager_RPS.py
@@ -19,19 +19,28 @@ from Autodesk.Revit.DB import *
 # Import RPW
 from rpw import ui
 from rpw.ui.forms import select_file, select_folder
+from rpw.ui.forms import (FlexForm, Label, ComboBox, TextBox, TextBox, Separator, Button)
 
 # Import Other
 import sys
 import csv
+import itertools
 
 doc = DocumentManager.Instance.CurrentDBDocument
 uiapp = DocumentManager.Instance.CurrentUIApplication
 app = uiapp.Application
 uidoc=DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
 
-# Start Transaction
-t = Transaction(doc, "Collect Line Styles")
-t.Start()
+# Open & Parse Standards File
+standardsList = []
+filepath = select_file('LineStyle Standards (*.csv)|*.csv', 'Select LineStyle Standards')
+with open(filepath) as csvfile:
+	reader = csv.reader(csvfile)
+	for row in reader:
+		standardsList.append(row)
+standardsList = list(itertools.chain(*standardsList))
+
+# Collect Line Styles from Active Document
 lineStyle = doc.Settings.Categories.get_Item(BuiltInCategory.OST_Lines)
 lineStyleSubTypes = lineStyle.SubCategories
 existingLines = []
@@ -42,20 +51,52 @@ for i in lineStyleSubTypes:
     existingLines.append(name)
     existingLineIds.append(ID)
 
-# End Transaction
-t.Commit()
-t.Dispose()
+# Check Standards Against Existing Line Styles
+nonconformList = []
+nonconformIdList = []
+conformList = []
+conformIdList = []
+comboBoxBuild = []
+for i in lineStyleSubTypes:
+    if i.Name not in standardsList:
+        nonconformList.append(i.Name)
+        nonconformIdList.append(i.Id)
+    else:
+        conformList.append(i.Name)
+        conformIdList.append(i.Id)
+        comboBoxBuild.append("'{}': '{}'".format(i.Name, i.Name))
 
-# Open & Parse Standards File
-standardsList = []
-filepath = select_file('LineStyle Standards (*.csv)|*.csv', 'Select LineStyle Standards')
-with open(filepath) as csvfile:
-	reader = csv.reader(csvfile)
-	for row in reader:
-		standardsList.append(row)
+# Collect Noncomforming Line Elements in Revit
+lineCollector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Lines).WhereElementIsNotElementType().ToElements()
+lineElementList = []
+nonconformLineElements = []
+nonconformLineElementNames = []
+nonconformLineElementNamesUQ = []
+flexFormBuild = []
+for i in lineCollector:
+    if i.LineStyle.Name in nonconformList:
+        # collect all nonconforming line elements from Revit
+        nonconformLineElements.append(i)
+        nonconformLineElementNames.append(i.LineStyle.Name)
+        if i.LineStyle.Name not in nonconformLineElementNamesUQ:
+            # add to unique names list
+            nonconformLineElementNamesUQ.append(i.LineStyle.Name)
+            # add unique names to flexform UI
+            flexFormBuild.append(eval("Label({})".format("'{}'".format(i.LineStyle.Name))))
+            flexFormBuild.append(eval("ComboBox('Box', {})".format("{'Opt 1': 10.0, 'Opt 2': 20.0}")))
 
-print standardsList
-# Check 
+# Flex Form
+components = flexFormBuild
+form = FlexForm('Line Style Manager', components)
+form.show()
+#test = form.values
 
 # Show Results
+print "Number of Line Styles in Current File: {}".format( len( existingLines ))
 print existingLines
+print "Number of Line Styles in Standards: {}".format( len( standardsList ))
+print standardsList
+print "Number of Nonconforming Line Styles: {}".format( len( nonconformList ))
+print nonconformList
+print "Number of Nonconforming Line Elements: {}".format( len( nonconformLineElements ))
+print nonconformLineElementNamesUQ
